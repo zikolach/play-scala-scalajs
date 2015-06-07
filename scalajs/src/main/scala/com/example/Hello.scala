f0c2de3b22540da8d23e30f1ef92e825598f11c8
Nikolay Kushin <zikolach@gmail.com>
2015-06-07 14:42:18 +0200

    - update dependencies - fix router

--- scalajs/src/main/scala/com/example/Hello.scala
+++ scalajs/src/main/scala/com/example/Hello.scala
@@ -1,48 +1,95 @@
 package com.example
 
-import japgolly.scalajs.react.{ReactComponentB, React}
+import org.scalajs.dom
+import scala.scalajs.js.JSApp
+import scala.scalajs.js.annotation.JSExport
+import japgolly.scalajs.react._, vdom.prefix_<^._, ScalazReact._
+import japgolly.scalajs.react.extra._
 import japgolly.scalajs.react.extra.router2._
 
-import scala.scalajs.js
-import scala.scalajs.js.annotation.JSExport
-import org.scalajs.dom
-import japgolly.scalajs.react.vdom.prefix_<^._
+object Hello extends JSApp {
 
-sealed trait Loc
+  implicit val baseUrl = BaseUrl.fromWindowOrigin_/
 
-case object Home extends Loc
+  sealed trait Loc
 
-object HomePage {
-  val component = ReactComponentB[RouterCtl[Loc]]("Home")
-  .render(ctl => {
-    <.div(
-      <.h1("Home")
-    )
-  }).build
-}
+  case object Home extends Loc
 
+  case object Login extends Loc
 
-object Hello extends js.JSApp {
+  object HomePage {
+    val component = ReactComponentB.static("Home",
+      <.div(
+        <.h1("Home")
+      )
+    ).buildU
+  }
+
+  object LoginPage {
+    val component = ReactComponentB.static("Login",
+      <.div(
+        <.h1("Login")
+      )
+    ).buildU
+  }
 
   val routerConfig = RouterConfigDsl[Loc].buildConfig { dsl =>
     import dsl._
 
-    (emptyRule
-      | staticRoute(root, Home)  ~> renderR(ctl => HomePage.component(ctl))
-      ) .notFound(redirectToPage(Home)(Redirect.Replace))
-  }.renderWith(layout)
+    (trimSlashes
+      | staticRoute(root, Home) ~> render(HomePage.component())
+      | staticRoute("#login", Login) ~> render(LoginPage.component())
+      )
+      .notFound(redirectToPage(Home)(Redirect.Replace))
+      .renderWith(layout)
+      .verify(Home, Login)
+  }
+
 
   def layout(c: RouterCtl[Loc], r: Resolution[Loc]) = {
-    <.div(^.className := "container")(r.render())
+    <.div(
+      navMenu(c),
+      <.div(^.cls := "container")(
+        r.render()
+      )
+    )
   }
+
+  val navMenu = ReactComponentB[RouterCtl[Loc]]("Menu")
+    .render { ctl =>
+
+    def nav(name: String, target: Loc) = {
+      val active = dom.window.location.hash == ctl.pathFor(target).value
+      <.li(^.cls := (if (active) "active" else ""),
+        ctl.link(target)(name)
+      )
+    }
+
+    <.nav(^.cls := "navbar navbar-default navbar-static-top")(
+      <.div(^.cls := "container")(
+        <.div(^.cls := "navbar-header")(
+          <.a(^.cls := "navbar-brand", ^.href := "#")(
+            <.img(^.alt := "Brand", ^.src := "assets/images/favicon.png")()
+          )),
+        <.div(^.cls := "collapse navbar-collapse")(
+          <.ul(^.cls := "nav navbar-nav")(
+            nav("Home", Home),
+            nav("Login", Login))
+        )
+      )
+    )
+  }
+    //    .configure(Reusability.shouldComponentUpdate)
+    .build
 
   @JSExport
   def main(): Unit = {
 
-    val baseUrl = BaseUrl.fromWindowOrigin
-    println(baseUrl)
-    val router = Router(baseUrl, routerConfig)
 
-    React.render(router(), dom.document.getElementById("root"))
+    dom.console.log(s"PATH: $baseUrl")
+
+    val router = Router(baseUrl, routerConfig.logToConsole)
+
+    router() render dom.document.body
   }
 }
